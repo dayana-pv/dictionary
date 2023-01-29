3e0b743410c9b73045a307f9954b51582e343403
dayana-pv <dayana.priscilla.17@gmail.com>
2023-01-29 18:33:10 -0500

    Added changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
@@ -7,7 +6,7 @@ export default function App() {
       <div className="container">
         <header className="App-header"></header>
         <main>
-          <Dictionary />
+          <h2>hola</h2>
         </main>
         <footer className="App-footer">
           <small>Coded by SheCodes</small>
